d323370d396800c4a4aced765783a3d63048a582
naphteine <58526116+naphteine@users.noreply.github.com>
2024-09-30 03:17:16 +0300

    Grid view images from cdn

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.gokay.works",
+        port: "",
+        pathname: "/studio/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
